ec207644f3ec991281d5ab82fed1a7cc521d27b8
Simon Lee <simon@leefamily.uk.net>
2025-06-29 19:28:38 +0100

    Add static file serving and create index.html for the web app

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 // Define a route for the homepage
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the Node.js Web Page!</h1>');
